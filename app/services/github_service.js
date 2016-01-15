5cb6ad2ba6e1926891173be5b1f50acc9a3aa2d0
Tomek Kuklis <tomasz.kuklis@gmail.com>
2016-01-15 11:36:10 +0100

    fix: remove access_token

--- app/services/github_service.js
+++ app/services/github_service.js
@@ -2,7 +2,7 @@ export default class GithubService {
   constructor($resource) {
     this.resource = $resource('https://api.github.com/repos/nukomeet/angular-workshop-example/issues/:number', {
       number: '@number',
-      access_token: "fa6e3d8a2f7c9ad26cea4a2783305bb0699fd52c"
+      /*access_token: ""*/
     })
   }
 
